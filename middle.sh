94907d8c64766ed669c90749d3e912b2f4062918
rovinghistorian <marcella@rovinghistorian.net>
2020-07-31 11:03:16 -0500

    Added metadata about variable order for script

--- middle.sh
+++ middle.sh
@@ -1,6 +1,11 @@
 
+
 # Add ability to set line numbers
 
 # Add ability to run on any file
+
+# $1 is the filename
+# $2 is the number of lines to send to head
+# $3 is the number of lines to send to tail
 
 head -n "$2" "$1" | tail -n "$3"
